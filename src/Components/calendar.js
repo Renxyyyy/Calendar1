a76a279a72f5ef0469211155628bd0c9014b88c7
Renxyyyy <131463199+Renxyyyy@users.noreply.github.com>
2023-04-22 09:18:01 +0800

    Add files via upload

---
+++ src/Components/calendar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Fullcalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import * as bootstrap from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+function Calendar() {
+  const events = [
+    {
+      title: "The Title",
+      start: "2023-04-05T08:00:00",
+      end: "2023-04-05T09:00:00",
+    },
+  ];
+  return (
+    <div>
+      <Fullcalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView={"dayGridMonth"}
+        headerToolbar={{
+          start: "today prev,next", // will normally be on the left. if RTL, will be on the right
+          center: "title",
+          end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
+        }}
+        height={"90vh"}
+        events={events}
+        eventDidMount={(info) => {
+          return new bootstrap.Popover(info.el, {
+            title: info.event.title,
+            placement: "auto",
+            trigger: "hover",
+            customClass: "default",
+            content:
+              "<p>Please subscribe<strong>Bootstrap popover</strong>.</p>",
+            html: true,
+          });
+        }}
+      />
+    </div>
+  );
+}
+
+export default Calendar;
